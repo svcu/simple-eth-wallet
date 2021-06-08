cdefb254fd88fd9fd8c7452e4520b234dc1b8511
Sergio Valdés Valdés <60680494+svcu@users.noreply.github.com>
2021-06-08 02:30:54 -0400

    Some improvements
    Migration to TypeScript of index.js, improved the database connection functionality, externalized configuration, and interaction with the Block Cypher API moved to the backend

--- config/db.js
+++ config/db.js
@@ -1,9 +1,23 @@
-var mongoose = require('mongoose');
-//Set up default mongoose connection
-var mongoDB = 'mongodb://localhost/simpleETH-Test3';
-mongoose.connect(mongoDB, { useNewUrlParser: true });
- //Get the default connection
-var db = mongoose.connection;
-//Bind connection to error event (to get notification of connection errors)
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+const mongoose = require("mongoose")
+require("dotenv").config({path: "./.env"})
+
+
+async function connection(){
+    try{
+        await mongoose.connect(process.env.DB.toString(), {useNewUrlParser: true})
+        
+        console.log("Succesfull connection")
+    }catch (e){
+        
+        console.log("Error on DB connection")
+        
+        process.exit(1)
+    }
+}
+
+connection();
+
+module.exports = {
+    connection
+}
+
